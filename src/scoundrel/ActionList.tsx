f2a36bd4660200b88dc2f6e4d8513f8549652b29
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-10-14 18:26:36 -0300

    Apply TS changes to Scoundrel action list

--- src/scoundrel/ActionList.tsx
+++ src/scoundrel/ActionList.tsx
@@ -1,4 +1,3 @@
-import { filterValid, isBlack, type Card } from "deck";
 import type { ScoundrelState } from "scoundrel/types";
 import styled from "styled-components";
 import tokens from "open-props";
@@ -6,6 +5,13 @@ import ActionButton from "ui/ActionButton";
 import { Text } from "ui/styles/typography";
 import colors from "ui/styles/colors";
 import { useGame } from "scoundrel/context";
+import flee from "scoundrel/moves/flee";
+import equipWeapon from "scoundrel/moves/equipWeapon";
+import fightBarehanded from "scoundrel/moves/fightBarehanded";
+import fightWithWeapon from "scoundrel/moves/fightWithWeapon";
+import drinkPotion from "scoundrel/moves/drinkPotion";
+import type { Move } from "framework/model/types";
+import type { Card } from "deck";
 
 const Wrapper = styled.div`
   gap: ${tokens.size2};
@@ -23,60 +29,12 @@ const Placeholder = styled.div`
   font-size: ${tokens.fontSize2};
 `;
 
-type ActionConfig = {
-  id: string;
-  label: string;
-  condition: (state: ScoundrelState, selected: Card | null) => boolean;
-  action: (state: ScoundrelState, selected: Card | null) => void;
-};
-
-const useWeapon: ActionConfig = {
-  id: "use-weapon",
-  label: "Use weapon",
-  condition: (_, selected) => !!selected && isBlack(selected),
-  action: (state, selected) => {
-    if (selected) state.fightWithWeapon(selected);
-  },
-};
-
-const fightBarehanded: ActionConfig = {
-  id: "fight-barehanded",
-  label: "Fight barehanded",
-  condition: (_, selected) => !!selected && isBlack(selected),
-  action: (state, selected) => {
-    if (selected) state.fightBarehanded(selected);
-  },
-};
-
-const drinkPotion: ActionConfig = {
-  id: "drink-potion",
-  label: "Drink",
-  condition: (_, selected) => selected?.suit === "heart",
-  action: (state, selected) => {
-    if (selected) state.drinkPotion(selected);
-  },
-};
-
-const equipWeapon: ActionConfig = {
-  id: "equip-weapon",
-  label: "Equip weapon",
-  condition: (_, selected) => selected?.suit === "diamond",
-  action: (state, selected) => {
-    if (selected) state.equipWeapon(selected);
-  },
-};
-
-const flee: ActionConfig = {
-  id: "flee",
-  label: "Flee",
-  condition: (state, selected) =>
-    !state.room.hasSkipped &&
-    filterValid(state.room.cards).length === 4 &&
-    !selected,
-  action: state => state.flee(),
-};
-
-const actions = [useWeapon, fightBarehanded, equipWeapon, drinkPotion, flee];
+const actions = [
+  ["Equip weapon", equipWeapon],
+  ["Fight barehanded", fightBarehanded],
+  ["Fight with weapon", fightWithWeapon],
+  ["Drink potion", drinkPotion],
+] as const;
 
 type Props = {
   selected: Card | null;
@@ -84,17 +42,25 @@ type Props = {
 };
 
 export default function ActionList({ selected, afterAction }: Props) {
-  const { state } = useGame();
-  const handle = (action: ActionConfig["action"]) => {
-    action(state, selected);
+  const { state, game } = useGame();
+  const handle = (action: Move<ScoundrelState, Card>) => {
+    if (!selected) return;
+    game.play(action(selected));
     afterAction();
   };
 
+  const handleFlee = () => {
+    game.play(flee());
+    afterAction();
+  };
+
+  if (!selected) return <ActionButton onClick={handleFlee}>Flee</ActionButton>;
+
   const ctas = actions
-    .filter(action => action.condition(state, selected))
-    .map(action => (
-      <ActionButton key={action.id} onClick={() => handle(action.action)}>
-        {action.label}
+    .filter(([, action]) => action.isValid(state, selected))
+    .map(([label, action]) => (
+      <ActionButton key={action.id} onClick={() => handle(action)}>
+        {label}
       </ActionButton>
     ));
 
